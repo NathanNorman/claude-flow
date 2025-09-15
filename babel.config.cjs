8b5791190c80a9493f663b805894b28150a36158
NathanNorman <nathannorman@gmail.com>
2025-09-15 00:03:44 -0400

    Clean initial commit: Claude Flow core functionality only

---
+++ babel.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '20',
+        },
+        modules: false,
+      },
+    ],
+  ],
+  plugins: ['@babel/plugin-syntax-import-attributes'],
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: '20',
+            },
+          },
+        ],
+      ],
+    },
+  },
+};
