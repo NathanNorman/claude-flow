63416c709730e366b4498ba927946b8468d70cf5
NathanNorman <nathannorman@gmail.com>
2025-09-15 09:57:50 -0400

    🔖 Checkpoint: Edit src/mle-star/ensemble_agent_implementation.py

--- src/mle-star/ensemble_agent_implementation.py
+++ src/mle-star/ensemble_agent_implementation.py
@@ -9,6 +9,7 @@ from sklearn.base import BaseEstimator
 from sklearn.model_selection import cross_val_predict
 from sklearn.linear_model import LogisticRegression, Ridge
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 import warnings
 
 warnings.filterwarnings("ignore")
